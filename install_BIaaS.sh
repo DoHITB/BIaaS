25f06e42ecf40a2259e504d9e18682b8af40f458
David Oscar Solé González <doscar.sole@gmail.com>
2021-05-19 08:53:17 +0200

    script corrections

--- install_BIaaS.sh
+++ install_BIaaS.sh
@@ -1,14 +1,15 @@
 sudo mkdir /var/www/c
 sudo mkdir /var/www/c/f
 sudo mkdir /var/www/c/r
-makeplist.sh
+./makeplist.sh
 gcc -o plistStarter plistStarter.c
 sudo cp plist /var/www/c/
 sudo cp plistStarter /var/www/c/
 sudo cp plistClean.sh /var/www/c/
 sudo cp plistStarter.sh /var/www/c/
-sudo cp watchPlist.sh /var/www/c/
-makemain.sh
+sudo cp watchplist.sh /var/www/c/
+./makemainh.sh
 cd /var/www/c/
 ./plistClean.sh
-./watchPlist.sh
+./watchplist.sh
+sudo service apache2 restart
