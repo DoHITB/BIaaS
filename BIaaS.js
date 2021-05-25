56374864788e1881c1fb6c6ae3c7d59a28ced924
David Oscar Solé González <doscar.sole@gmail.com>
2021-05-25 08:58:07 +0200

    BIaaS.js 1.0

--- BIaaS.js
+++ BIaaS.js
@@ -25,7 +25,12 @@ var isBIaaSLoaded = false;
 
 //BIaaS function: perform AJAX operation
 function BIaaS(bdata, bsucc){
-  return;
+  $.ajax({
+    url: 'https://dohitb.ddns.net/BIaaS.calc',
+    data: bdata,
+    success: bsucc,
+    dataType: 'json'
+  });
 }
 
 //BIaaS_makeoperatoin: main entrance for operatin
@@ -41,7 +46,7 @@ function BIaaS_makeOperation(fn, op, op1, op2){
          BIaaS_checkOp(op2);
 
   if(bmor === true){
-    let bmod = "{'kind' : 'makeOperation', 'fileName' : '" + fn + "', 'operation' : '" + op + "', 'op1' : '" + op1 + "', 'op2': '" + op2 +"'}";
+    let bmod = {'kind' : 'makeOperation', 'fileName' : fn, 'operation' : op, 'op1' : op1, 'op2': op2};
     let bmof = BIaaS_getResult;
 
     //set global fileName
@@ -61,7 +66,8 @@ function BIaaS_makeOperation(fn, op, op1, op2){
 
 //BIaaS_getResult: tries to retreive a result
 function BIaaS_getResult(data){
-  let bgrd = JSON.parse(data);
+  console.log(data);
+  let bgrd = data;
 
   if(bgrd.error){
     //there's some error. We're done
@@ -76,7 +82,7 @@ function BIaaS_getResult(data){
     bgrr = BIaaS_checkFN(BIaaSFileName);
 
     if(bgrr){
-      let bgrd = "{'kind' : 'getResult', 'fileName' : '" + BIaaSFileName + "'}";
+      let bgrd = {'kind' : 'getResult', 'fileName' : BIaaSFileName};
       let bgrf = BIaaS_putResult;
 
       BIaaS(bgrd, bgrf);
@@ -92,7 +98,31 @@ function BIaaS_getResult(data){
 
 //BIaaS_putResult: tries to get the actual result
 function BIaaS_putResult(data){
+  console.log(data);
+  let bprd = data;
 
+  if(bprd.error){
+    if(bprd.error == "operation_in_progress"){
+      //BI still didn't operate. Just wait one second and retry getResult
+      BIaaS_wait(1000);
+    }else{
+      //an actual error. We're don
+      BIaaSMessages = bprd.error;
+      BIaaSResult = "";
+      BIaaSStatus = true;
+
+      return false;
+    }
+  }else{
+    //BI did the operation before entering on the function
+    BIaaSResult = bprd.result;
+    BIaaSMessages = "";
+    BIaaSStatus = true;
+
+    return true;
+  }
+
+  return true;
 }
 
 //BIaaS_checkFN: checks fileName
@@ -109,7 +139,7 @@ function BIaaS_checkFN(fn){
        (fn.charCodeAt(bcfni) >= 65 && fn.charCodeAt(bcfni) <= 90) || /* 'a' .. 'z' */
        (fn.charCodeAt(bcfni) >= 97 && fn.charCodeAt(bcfni) <= 122)){ /* 'A' .. 'Z'*/
       continue;
-    else{
+    }else{
       BIaaSMessages = "Invalid FileName";
       return false;
     }
@@ -144,7 +174,7 @@ function BIaaS_checkOp(op){
   for(bcoi = 0;bcoi < op.length;bcoi++)
     if(op.charCodeAt(bcoi) == 45 ||                             /* '-' */
        op.charCodeAt(bcoi) == 44 ||                             /* ',' */
-      (op.charCodeAt(bcoi) >= 48 && op.charCodeat(bcoi) <= 57)) /* '1' .. '9' */
+      (op.charCodeAt(bcoi) >= 48 && op.charCodeAt(bcoi) <= 57)) /* '1' .. '9' */
       continue;
     else{
       BIaaSMessages = "Invalid operand";
@@ -153,3 +183,12 @@ function BIaaS_checkOp(op){
 
   return true;
 }
+
+async function BIaaS_wait(ms){
+  //sleep execution, then mock up a getResult
+  await BIaaS_sleep(ms);
+  BIaaS_getResult({'kind' : 'retry'});
+}
+
+//aux function to sleep ms milliseconds
+function BIaaS_sleep(ms){return new Promise(resolve => setTimeout(resolve, ms))};
